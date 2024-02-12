740a7c6a9d5b997ea0656ee951e640cb413f6baf
Avijit Sarkar <avijitsarkar2222@gmail.com>
2024-02-12 20:44:11 +0530

    login and register

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import Layout from "components/Layout";
 import Home from "pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "pages/Login";
+import Register from "pages/Register";
+import Classes from "pages/Classes";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-          {/* <Route path="/" element={<Home />}> */}
+          {/* <Route path="/" element={<LandingPage />}> */}
 
-          <Route index element={<Home />}></Route>
-          {/* <Route path="account" element={<Account />} /> */}
+          <Route index element={<Home />} />
+          <Route path="classes" element={<Classes />} />
           {/* <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} /> */}
         </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
